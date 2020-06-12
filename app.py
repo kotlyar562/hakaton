9f6020b8fb84ba40768bdfeaa1f9118dd7098dfb
Fol1Ne <seleznevandrei163@gmail.com>
2020-06-12 15:06:51 +0100

    Created html pages, app.py and linked style.css to base.html

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return 'Hello'
+def mainPage():
+    return render_template("mainPage.html")
+
+@app.route('/question1')
+def question1():
+    return render_template("question1.html")
+
+@app.route('/question2')
+def question2():
+    return render_template("question2.html")
+
+@app.route('/question3')
+def question3():
+    return render_template("question3.html")
